fa968421f9ed00e4fffc8161f56ee5f8343f4dd4
Guru Shankar <guru03@outlook.com>
2025-12-27 10:02:44 +0530

    basic file installation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Main } from './main/main';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+    path: '',
+    component: Main,
+    // children: [
+    //   { path: '', redirectTo: 'list/' },
+    // ],
+  },
+];
